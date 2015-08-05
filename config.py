85dc86e99578c007b0f872590a10ee42c34c707a
Alexander Luberg <alex@luberg.me>
2015-08-05 09:23:44 -0700

    Send nicely formatted emails with results + a config

---
+++ config.py
@@ -0,0 +1,9 @@
+MAILGUN_SERVER = ''
+MAILGUN_KEY = ''
+SEND_NOTIFICATIONS_TO = MAILGUN_EMAIL_SENDER = ''
+MAILGUN_URL = 'https://api.mailgun.net/v2/{server}/messages'.format(server=MAILGUN_SERVER)
+EMAIL_SUBJECT = 'New Houses'
+
+SEARCH_FEEDS = [
+
+]
--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from operator import attrgetter
 from collections import namedtuple
 from multiprocessing.pool import Pool
 
+from jinja2 import Environment, FileSystemLoader
+import requests
 import feedparser
 
 import config
 
-SearchResult = namedtuple('SearchResult', ['title', 'link'])
+SearchResult = namedtuple('SearchResult', ['title', 'url'])
 
 pool = Pool(5)
 
@@ -16,3 +18,17 @@ if __name__ == '__main__':
     entries = chain.from_iterable(map(attrgetter('entries'), feeds))
     unique_entries = dict((v['link'], v) for v in entries).values()
     results = (SearchResult(entry.title, entry.link) for entry in unique_entries)
+
+    if results:
+        env = Environment(autoescape=True, loader=FileSystemLoader('templates'))
+        template = env.get_template('notification.html')
+        email_msg = template.render(title=config.EMAIL_SUBJECT, results=results)
+
+        requests.post(config.MAILGUN_URL,
+                      auth=("api", config.MAILGUN_KEY),
+                      data={
+                          "from": config.MAILGUN_EMAIL_SENDER,
+                          "to": config.SEND_NOTIFICATIONS_TO,
+                          "subject": config.EMAIL_SUBJECT,
+                          "html": email_msg
+                      })
